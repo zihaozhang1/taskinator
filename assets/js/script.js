5b25c1905ee9a4853e84c625eb3585199e115ee7
Zihao <zihaozhang1@gmail.com>
2020-11-05 01:27:26 -0500

    added the ability to add tasks

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+buttonEl.addEventListener("click", createTaskHandler);
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
