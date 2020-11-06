84ae61bce7f7f99c6c73fe9c81fd3384f47c7a1e
Zihao <zihaozhang1@gmail.com>
2020-11-05 23:58:43 -0500

    added handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,18 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 console.log(buttonEl);
-buttonEl.addEventListener("click", createTaskHandler);
+
 
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
+var createTaskHandler = function(event) {
 
-var createTaskHandler = function() {
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  }
+  };
+
+  formEl.addEventListener("submit", createTaskHandler);
